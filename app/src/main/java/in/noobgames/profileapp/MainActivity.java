ba82684e74c0defc6fcc528f8ade582e390d9054
Abhishek Jain <abhi@noobgames.in>
2016-05-09 13:01:35 +0530

    Added Ads

--- app/src/main/java/in/noobgames/profileapp/MainActivity.java
+++ app/src/main/java/in/noobgames/profileapp/MainActivity.java
@@ -5,16 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder()
+                .addTestDevice("A3E4D415D706C04E648BF60396FD07B5")
+                .build();
+        adView.loadAd(request);
     }
 
     public void onMapClick(View view) {
-        Intent intent = new Intent(this,MapsActivity.class);
+        Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
 }
